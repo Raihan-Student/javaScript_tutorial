e83ed8aaf81fddb68477652268afed4f4701660f
Abu Raihan Mondal <raihanmondal938207@gmail.com>
2023-11-15 09:18:08 +0530

    add new files

---
+++ index.js
@@ -0,0 +1,15 @@
+/*console.log("Allahu Akbar");
+console.log("La ilaha illah");
+window.alert("Allahu Akbar");
+*/
+let first_name = "Raihan";
+let age = 23;
+let student = true;
+
+
+console.log(first_name);
+console.log("you are ",age,"year old");
+console.log("Enrolled:",student);
+document.getElementById("p1").innerHTML = "Assalamu walikum "+first_name;
+document.getElementById("p2").innerHTML = "You are "+ age + " year old";
+document.getElementById("p3").innerHTML = "Enrolled : "+student;
